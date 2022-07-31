4fcf3f4677215982989e9cafee4ad0316cf34b3d
Muzaffer Kaplan <muzafferckaplan@gmail.com>
2022-07-31 11:41:00 +0300

    Footers buttons are active

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import List from './components/List/List'
 import "./App.css"
 import Footer from './components/Footer/Footer'
 
-const initial = []
+
 const App = () => {
 
-  const [task,setTask] =useState(initial)
+  const [task,setTask] =useState([])
+  const [filter, setFilter] = useState('all')
+
+  // const handleStatus = (type) => {
+  //   if (type == "active") {
+  //     setFilter("active")
+  //   } else if (type == "completed") {
+  //     setFilter("completed")
+  //   } else {
+  //     setFilter("all")
+  //   }
+  // }
+  
   return (
     <div className='app-container'>
   
      <h2 className='app-header'>Todos</h2>
       <Form setTask={setTask} task={task}/>
-      <List task={task} setTask={setTask}/>
+      <List task={task} setTask={setTask} filter={filter}/>
      
-      <Footer  task={task} setTask={setTask}/>
+      <Footer  task={task} setTask={setTask} handleStatus={setFilter}/>
       
     </div>
   )
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,20 +1,33 @@
+import React from "react"
 import "./List.css"
 
-const List = ({ task, setTask }) => {
+
+const List = ({ task, setTask, filter }) => {
     const handleDelete = (i) => {
         const tasks = [...task]
-        tasks.splice(i,1)
+        tasks.splice(i, 1)
         setTask(tasks)
     }
+
     return (
         <div className={task.length === 0 ? "emptycontainer" : "container"}>
-            {task.map((item, i) =>
-                <div key={i} className="list-container">
+            {task.filter((item) => {
+                if (filter === "completed" && !item.done) {
+                    return true
+                } else if (filter === "active" && item.done) {
+                    return true
+                } else if (filter === "all") {
+                    return item
+                }
+            }).map((item, index) =>
+                <div key={index} className="list-container">
                     <span onClick={() => setTask(task.map(e => e.id === item.id ? { ...e, done: !e.done } : e))}
                         className={item.done ? "doneWork" : "workDone"}>{item.work}</span>
-                    <span onClick={() => handleDelete(i)} className="delete">X</span>
+                    <span onClick={() => handleDelete(index)} className="delete">X</span>
                 </div>
-            )}
+            )
+            }
+
         </div>
     )
 }
